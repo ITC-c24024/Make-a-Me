bc6f4e2b1f36fb65833103f41bdb1a56348ecfa5
祐貴 前田 <c24024@std.it-college.ac.jp>
2025-09-04 15:29:45 +0900

    スタン処理を仮実装

--- Assets/Maeda/Scripts/PlayerController.cs
+++ Assets/Maeda/Scripts/PlayerController.cs
@@ -20,6 +20,11 @@ public class PlayerController : ActionScript
     //バッテリーの所持判定
     public bool haveBattery = false;
 
+    [SerializeField, Header("スタン効果時間")]
+    float stanTime = 2.0f;
+    //スタン判定
+    bool isStan = false;
+
     void Start()
     {
         var gameController = GameObject.Find("GameController").GetComponent<GameController>();
@@ -67,8 +72,22 @@ public class PlayerController : ActionScript
         StartCoroutine(PickupDelay());
     }
 
+    /// <summary>
+    /// バッテリー所持判定を切り替え
+    /// </summary>
     public void ChangeHaveBattery()
     {
         haveBattery = !haveBattery;
+    }
+
+    /// <summary>
+    /// スタン処理
+    /// </summary>
+    /// <returns></returns>
+    public IEnumerator Stan()
+    {
+        isStan = true;
+        yield return new WaitForSeconds(stanTime);
+        isStan = false;
     }
 }
