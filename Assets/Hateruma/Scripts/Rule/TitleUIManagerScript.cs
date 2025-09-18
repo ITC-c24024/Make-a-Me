7e7aa28812b43389873ecc471c43fc1ff6e8baa4
煌斗 波照間 <c24021@std.it-college.ac.jp>
2025-09-18 17:20:45 +0900

    タイトル処理途中

--- Assets/Hateruma/Scripts/Rule/TitleUIManagerScript.cs
+++ Assets/Hateruma/Scripts/Rule/TitleUIManagerScript.cs
@@ -6,6 +6,8 @@ using UnityEngine.UI;
 
 public class TitleUIManagerScript : UIManagerScript
 {
+    [SerializeField, Header("タイトルロゴ")]
+    Image titleImage;
 
     [SerializeField, Header("タイトルのボタンImage")]
     Image[] buttonImage;
@@ -19,9 +21,17 @@ public class TitleUIManagerScript : UIManagerScript
     [SerializeField, Header("操作方法画面の戻るUI")]
     Image returnControllPanel;
 
+    [SerializeField]
+    AudioManager audioManager;
+
+    [SerializeField]
+    ShutterScript shutterScript;
+
     int selectNum;
 
     bool canSelect;
+
+    bool isTitle = true;
 
     void Start()
     {
@@ -32,14 +42,19 @@ public class TitleUIManagerScript : UIManagerScript
     {
         Vector2 stickMove = selectAction.ReadValue<Vector2>();
 
-        if (!isCoolTime && canSelect)
+        if (isTitle && decisionAction.triggered)
+        {
+            StartCoroutine(HideTitle());
+        }
+
+        if (!isCoolTime && canSelect && !isTitle)
         {
             if (stickMove.y > 0.2f) ChangeSelect(-1); // 上
             if (stickMove.y < -0.2f) ChangeSelect(1); // 下
             if (decisionAction.triggered) Decision();
 
         }
-        else if (!isCoolTime && !canSelect)
+        else if (!isCoolTime && !canSelect && !isTitle)
         {
             if (stickMove.y > 0.2f || stickMove.y < -0.2f)
             {
@@ -67,8 +82,8 @@ public class TitleUIManagerScript : UIManagerScript
         // 移動
         selectNum += direction;
 
-        if (selectNum < 0) selectNum = buttonImage.Length-1;
-        if (selectNum > buttonImage.Length-1) selectNum = 0;
+        if (selectNum < 0) selectNum = buttonImage.Length - 1;
+        if (selectNum > buttonImage.Length - 1) selectNum = 0;
 
         // 新しい選択をON
         if (selectNum < buttonImage.Length)
@@ -105,7 +120,9 @@ public class TitleUIManagerScript : UIManagerScript
         switch (selectNum)
         {
             case 0:
-                SceneManager.LoadScene("MainScene");
+                audioManager.TitleStop();
+                StartCoroutine(shutterScript.CloseShutter());
+                Invoke(nameof(MainScene), 2.5f);
                 canSelect = false;
                 break;
 
@@ -128,5 +145,29 @@ public class TitleUIManagerScript : UIManagerScript
     public void SelectOK()
     {
         canSelect = true;
+    }
+
+    void MainScene()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    IEnumerator HideTitle()
+    {
+        float count = 0f;
+        Color startColor = titleImage.color;
+
+        while (count < 1)
+        {
+            count += Time.deltaTime;
+            float alpha = Mathf.Lerp(1f, 0f, count / 1);
+            titleImage.color = new Color(startColor.r, startColor.g, startColor.b, alpha);
+            yield return null;
+        }
+
+        // 完全に透明にする
+        titleImage.color = new Color(startColor.r, startColor.g, startColor.b, 0f);
+
+        
     }
 }
