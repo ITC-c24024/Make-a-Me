ee48795ae4ac561423928092bbaab27ddae783be
煌斗 波照間 <c24021@std.it-college.ac.jp>
2025-09-19 18:01:02 +0900

    リザルト演出もうちょい

--- Assets/Hateruma/Scripts/Result/DispencerScript.cs
+++ Assets/Hateruma/Scripts/Result/DispencerScript.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+using UnityEngine.UI;
 
 public class DispencerScript : MonoBehaviour
 {
@@ -10,23 +12,37 @@ public class DispencerScript : MonoBehaviour
     [SerializeField, Header("発射オブジェクト")]
     GameObject shotObj;
 
-    [SerializeField, Header("クローンの色")]
-    string cloneColor;
+    [SerializeField, Header("プレイヤー番号")]
+    int playerNum;
 
     [SerializeField] List<GameObject> cloneObj; // SerializeField を残す
     List<Rigidbody> cloneRB;
 
-    [SerializeField,Header("クローンの数")]
+    [SerializeField, Header("クローンの数")]
     int cloneCount;
 
+    //発射数
+    int shotCount;
 
+    [SerializeField, Header("順位イメージ")]
+    Image[] rankImage;
 
+    [SerializeField, Header("カウントのイメージ")]
+    Image[] countImage;
+
+    [SerializeField, Header("数字のスプライト")]
+    Sprite[] numSprite;
+
+    ScoreManager scoreManaSC;
     private void Awake()
     {
+        //scoreManaSC = GameObject.Find("ScoreManager").GetComponent<ScoreManager>();
         if (cloneObj == null) cloneObj = new List<GameObject>();
         if (cloneRB == null) cloneRB = new List<Rigidbody>();
 
-        GameObject[] clones = GameObject.FindGameObjectsWithTag($"ResultClone{cloneColor}");
+        //cloneCount = scoreManaSC.players[playerNum - 1].score;
+
+        GameObject[] clones = GameObject.FindGameObjectsWithTag($"ResultCloneP{playerNum}");
         cloneObj.AddRange(clones);
     }
 
@@ -65,13 +81,38 @@ public class DispencerScript : MonoBehaviour
             cloneRB.RemoveAt(0);
 
             cloneCount--;
+            shotCount++;
 
             if (count > 0.1f)
             {
-                count -= 0.005f;
+                count -= 0.01f;
             }
+
+            SetUI();
 
             yield return new WaitForSeconds(count);
         }
+    }
+
+    void SetUI()
+    {
+        int ten = shotCount / 10; //十の位
+        int one = shotCount - 10 * ten; //一の位
+
+        //十の位がある時,十の位が表示されていないとき
+        if (ten > 0 && !countImage[1].enabled)
+        {
+            Debug.Log("hoge");
+            //一の位を右にずらす
+            countImage[0].rectTransform.anchoredPosition = new Vector2(
+                countImage[0].rectTransform.anchoredPosition.x + 20,
+                countImage[0].rectTransform.anchoredPosition.y
+                );
+            //十の位を表示
+            countImage[1].enabled = true;
+        }
+
+        countImage[0].sprite = numSprite[one];
+        countImage[1].sprite = numSprite[ten];
     }
 }
